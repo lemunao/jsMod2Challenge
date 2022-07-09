b0ff9ddd3248e5b27f2b14bc6ec7aa595453fd32
lemunao <pedroandres1@gmail.com>
2022-07-09 12:05:27 -0400

    End second challenge javascript

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -2,8 +2,9 @@
 //Create pointer
 var cont = 0;
 //Make query selector to find event onclick and create function to make validations
+
 document.querySelector('#marioImg').onclick = function () {
-    var image = document.querySelector('#marioImg');    
+    var image = document.querySelector('#marioImg');
     //Start condition
     if (cont == 0) {
         //Added atributes to image
@@ -14,8 +15,25 @@ document.querySelector('#marioImg').onclick = function () {
         //remove properties
         image.style.removeProperty("border");
         //pointer = 0
-        cont = 0; 
+        cont = 0;
     }
 }
 
 // Items Controll, Challenge 2
+function validateElements() {
+    let inputMario = parseInt(document.querySelector('#inputMario').value)
+    let inputHomer = parseInt(document.querySelector('#inputHomer').value)
+    let inputPuppys = parseInt(document.querySelector('#inputPuppys').value)
+    sumElement = inputMario + inputHomer + inputPuppys
+    if (sumElement <= 10 && isNaN(sumElement) == false) {
+        document.querySelector('#message').innerHTML = "Llevas " + sumElement;
+        return;
+    } if (isNaN(sumElement)) {
+        document.querySelector('#message').innerHTML = "Favor solo ingresar valor númericos ";
+        return;
+    }
+    else {
+        document.querySelector('#message').innerHTML = "Llevas demasiados sticket, máximo es 10. Llevas " + sumElement;
+        return;
+    }
+}
